150f188695d6e748faeb6725eaa6c36e29fdbd74
Harshit Mehra <141059935+HarshitMehraa@users.noreply.github.com>
2025-07-31 09:34:20 +0530

    Add files via upload

---
+++ Query for QN 8.sql
@@ -0,0 +1,17 @@
+-- In which quarter of 2020, got the maximum total_sold_quantity? 
+-- The final output contains these fields sorted by the total_sold_quantity, Quarter, total_sold_quantity
+
+SELECT 
+    CASE
+        WHEN MONTH(date) IN (01 , 02, 03) THEN 'Q1'
+        WHEN MONTH(date) IN (04 , 05, 06) THEN 'Q2'
+        WHEN MONTH(date) IN (07 , 08, 09) THEN 'Q3'
+        WHEN MONTH(date) IN (10 , 11, 12) THEN 'Q4'
+    END AS Quarter,
+    SUM(sold_quantity) AS total_sold_quantity
+FROM
+    fact_sales_monthly
+WHERE
+    fiscal_year = 2020
+GROUP BY Quarter
+ORDER BY total_sold_quantity DESC;
